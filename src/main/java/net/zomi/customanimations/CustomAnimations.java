3da3459d276b483ba9dd20eec5b2895f4f7f6d94
zomi <mikolajpietrzakowski3659@gmail.com>
2025-07-03 01:53:33 +0200

    file

---
+++ src/main/java/net/zomi/customanimations/CustomAnimations.java
@@ -0,0 +1,15 @@
+package net.zomi.customanimations;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CustomAnimations implements ModInitializer {
+	public static final String MOD_ID = "custom-animations";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/zomi/customanimations/CustomAnimationsClient.java
@@ -0,0 +1,10 @@
+package net.zomi.customanimations;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CustomAnimationsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
